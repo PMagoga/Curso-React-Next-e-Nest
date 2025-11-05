e1cacb9ed8f7d2894fb5042e2a0234fb2fd61a73
PMagoga <pmagoga@gmail.com>
2025-11-04 22:21:09 -0300

    finalizando context

--- chronos-pomodoro/src/App.tsx
+++ chronos-pomodoro/src/App.tsx
@@ -1,21 +1,8 @@
-import { TaskContextProvider } from "./contexts/TaskContext";
-import type { TaskStateModel } from "./models/TaskStateModel";
+import { TaskContextProvider } from "./contexts/TaskContext/TaskContextProvider";
 import { Home } from "./pages/Home";
+
 import "./styles/global.css";
 import "./styles/theme.css";
-
-const initialState: TaskStateModel = {
-  tasks: [],
-  secondsRemaining: 0,
-  formattedSecondsRemaining: "00:00",
-  activeTask: null,
-  currentCycle: 0,
-  config: {
-    workTime: 25,
-    shortBreakTime: 5,
-    longBreakTime: 15,
-  },
-};
 
 export default function App() {
   return (
